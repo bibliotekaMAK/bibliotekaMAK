b317a686061510c66513ab3df3696d661b1a055f
Mariusz <m.purgal@gmail.com>
2011-05-10 21:25:18 +0200

    Strona dla pracowników. Dodano podświetlenie wyszukiwanych fraz.
    Signed-off-by: Mariusz <m.purgal@gmail.com>

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
-<?php
-session_start();
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!--
 Tytu³ ; Biblioteczka MAK - System zarz±dzania - Biblioteka
 Opis  ; Strona g³ówna dostêpna dla internautów, na której ka¿dy mo¿e przejrzeæ
-       zawarto¶æ bazy, korzystaj±c z wyszukiwarki. Jest tutaj te¿ informacja
-       o nowo¶ciach, a tak¿e ilo¶æ ksi±¿ek w bazie.	
+zawarto¶æ bazy, korzystaj±c z wyszukiwarki. Jest tutaj te¿ informacja
+o nowo¶ciach, a tak¿e ilo¶æ ksi±¿ek w bazie.	
 Data  ; Marzec 2011
 Autor ; Mariusz P., Agnieszka S., Krzysztof P.
 -->
@@ -20,7 +17,7 @@ Autor ; Mariusz P., Agnieszka S., Krzysztof P.
   <link href="default.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
-<?php include("plikconfig.php");?>
+  <?php include("plikconfig.php");?>
   <noscript>
     <p class="noscriptText"><b>Koniecznie w³±cz obs³ugê JavaScript.</b><a href="http://www.kropleinternetu,biz/obslugajava.php" title="W³±czenie ods³ugi JavaScript w przegl±darkach internetowych." target="_blank" style="text-decoration:blink;"><b>Kliknij i sprawd¼ jak to zrobiæ&nbsp;&nbsp;&raquo;</b></a></p>
   </noscript>
@@ -28,31 +25,64 @@ Autor ; Mariusz P., Agnieszka S., Krzysztof P.
     <h1 style="text-transform:none;">Biblioteka Miejska nr.3 w Demo</h1>
     <h2>zapraszamy</h2>
   </div>
-  <div id="menu">
-   &nbsp;
-  </div>
+  <div id="menu">&nbsp;</div>
   <div id="content">
-    <div id="columnA">
- <?php // Odczyt z bazy danych sk³ada siê z przy³±czenia siê do MySQL,
-       // wybrania bazy danych, zadania pytania SQL i przetworzenia wyników.
- 
- $link = mysql_connect ("$dbhost", "$dblogi", "$dbpass") or 
- die ("Nie mo¿na po³±czyæ siê z MySQL");
- mysql_select_db ("$dbname") or 
- die ("Nie mo¿na po³±czyæ siê z baz± biblioteki");
- 
- 
-  ?>
+    <div id="columnA"><?php // Odczyt z bazy danych sk³ada siê z przy³±czenia siê do MySQL,
+      // wybrania bazy danych, zadania pytania SQL i przetworzenia wyników.
+      
+      $link = mysql_connect ("$dbhost", "$dblogi", "$dbpass") or 
+      die ("Nie mo¿na po³±czyæ siê z MySQL");
+      mysql_select_db ("$dbname") or 
+      die ("Nie mo¿na po³±czyæ siê z baz± biblioteki");
+      
+      
+      ?>
       <h2>Wyszukaj ksi±¿ki</h2>
       <p>
       <form method="post" action="index.php">
         <hr style="border:1px solid #274690;" />
         Szukaj:
-        <input type="text" name="fraza" value="">
+        <input type="text" name="fraza" style="color:green;" value="2000">
         <input type="submit" value="Szukaj" /><a href="./">Powrót do wyszukiwarki</a>
         <hr style="border:1px solid #274690;" />
       </form>
-      <h2>Nie wpisano ¿adnej frazy do wyszukania</h2>
+      <table>
+      <tr>
+        <td><b>Id</b></td>
+        <td><b>Tytu³</b></td>
+        <td><b>Autor</b></td>
+        <td><b>ISBN</b></td>
+        <td><b>Rok wydania</b></td>
+      </tr>
+      <tr>
+        <td>56</td>
+        <td>DEWAJTIS</td>
+        <td>RODZIEWICZOWNA MARIA</td>
+        <td>999958</td>
+        <td><span style="background:#FFFF80;">2000</span></td>
+      </tr>
+      <tr>
+        <td>20</td>
+        <td>DISNEYLAND</td>
+        <td>DYGAT STANIS?AW</td>
+        <td>999994</td>
+        <td><span style="background:#FFFF80;">2000</span></td>
+      </tr>
+      <tr>
+        <td>30</td>
+        <td>MODLITWA ZABY</td>
+        <td>ANTHONY DE MELLO</td>
+        <td>999984</td>
+        <td><span style="background:#FFFF80;">2000</span></td>
+      </tr>
+      <tr>
+        <td>41</td>
+        <td>POTOP</td>
+        <td>SIENKIEWICZ HENRYK</td>
+        <td>999973</td>
+        <td><span style="background:#FFFF80;">2000</span></td>
+      </tr>
+      </table>
       </p>
       <h2>~ Nowo¶ci w bibliotece ~ ostatnie 10</h2>
       <p>
@@ -69,129 +99,128 @@ Autor ; Mariusz P., Agnieszka S., Krzysztof P.
       </tr>
       <tr>
         <td><b>1.</b></td>
-        <td>14</td>
-        <td>CZERWONY KAPTUREK</td>
-        <td>W. WILKOWIEC</td>
-        <td>234231</td>
-        <td>2010</td>
+        <td>57</td>
+        <td>OGROD WEZA</td>
+        <td>JUDITH MERKLE RILEY</td>
+        <td>837132</td>
+        <td>1998</td>
         <td>1</td>
         <td>1</td>
       </tr>
       <tr>
         <td><b>2.</b></td>
-        <td>13</td>
-        <td>PULAPKA DANTEGO</td>
-        <td>A. DELALANDE</td>
-        <td>978832</td>
-        <td>2007</td>
-        <td>1</td>
+        <td>56</td>
+        <td>DEWAJTIS</td>
+        <td>RODZIEWICZOWNA MARIA</td>
+        <td>999958</td>
+        <td>2000</td>
         <td>3</td>
-      </tr>
-      <tr>
-        <td><b>3.</b></td>
-        <td>12</td>
-        <td>MESALINA</td>
-        <td>GUY RACHET</td>
-        <td>676554</td>
-        <td>1993</td>
-        <td>1</td>
         <td>2</td>
       </tr>
       <tr>
+        <td><b>3.</b></td>
+        <td>55</td>
+        <td>PAN TADEUSZ</td>
+        <td>MICKIEWICZ ADAM</td>
+        <td>999959</td>
+        <td>1993</td>
+        <td>4</td>
+        <td>0</td>
+      </tr>
+      <tr>
         <td><b>4.</b></td>
-        <td>11</td>
-        <td>TAJEMNICZA WYPRAWA</td>
-        <td>A. SZKLARSKI</td>
-        <td>230995</td>
+        <td>54</td>
+        <td>LUDZIE BEZDOMNI</td>
+        <td>ZEROMSKI STEFAN</td>
+        <td>999960</td>
         <td>1992</td>
         <td>3</td>
         <td>2</td>
       </tr>
       <tr>
         <td><b>5.</b></td>
-        <td>10</td>
-        <td>LINUX CZARNA KSIEGA</td>
-        <td>H. TSUJI</td>
-        <td>788987</td>
+        <td>53</td>
+        <td>POLSKIE PANSTWO PODZIEMNE</td>
+        <td>KORBONSKI STEFAN</td>
+        <td>999961</td>
         <td>2008</td>
         <td>1</td>
-        <td>4</td>
+        <td>1</td>
       </tr>
       <tr>
         <td><b>6.</b></td>
-        <td>8</td>
-        <td>OBSESJA</td>
-        <td>S.LEWIS</td>
-        <td>223325</td>
+        <td>52</td>
+        <td>CZERWONE I CZARNE</td>
+        <td>STENDHALL</td>
+        <td>999962</td>
         <td>2009</td>
-        <td>1</td>
+        <td>5</td>
         <td>3</td>
       </tr>
       <tr>
         <td><b>7.</b></td>
-        <td>7</td>
-        <td>PROGRAMOWANIE PHP</td>
-        <td>A. KIERKOWSKA</td>
-        <td>154345</td>
-        <td>2007</td>
+        <td>51</td>
+        <td>PLACOWKA</td>
+        <td>PRUS BOLESLAW</td>
+        <td>999963</td>
+        <td>1989</td>
         <td>2</td>
         <td>4</td>
       </tr>
       <tr>
         <td><b>8.</b></td>
-        <td>5</td>
-        <td>DOM NA SEKWANIE</td>
-        <td>WHARTON WILLIAM</td>
-        <td>112345</td>
-        <td>2003</td>
-        <td>2</td>
-        <td>8</td>
+        <td>50</td>
+        <td>WESELE</td>
+        <td>WYSPIANSKI STANISLAW</td>
+        <td>999964</td>
+        <td>1991</td>
+        <td>4</td>
+        <td>6</td>
       </tr>
       <tr>
         <td><b>9.</b></td>
-        <td>4</td>
-        <td>KAMIENIE NA SZANIEC</td>
-        <td>A. KAMINSKI</td>
-        <td>837132</td>
+        <td>49</td>
+        <td>STARY CZLOWIEK I MORZE</td>
+        <td>HEMINGWAY ERNEST</td>
+        <td>999965</td>
         <td>1990</td>
-        <td>1</td>
+        <td>3</td>
         <td>2</td>
       </tr>
       <tr>
         <td><b>10.</b></td>
-        <td>3</td>
-        <td>OSOBA I CZYN</td>
-        <td>KAROL WOJTYLA</td>
-        <td>345678</td>
-        <td>1998</td>
-        <td>2</td>
+        <td>48</td>
+        <td>KOMU BIJE DZWON</td>
+        <td>HEMINGWAY ERNEST</td>
+        <td>999966</td>
+        <td>1997</td>
+        <td>4</td>
         <td>2</td>
       </tr>
       </table>
       </p>
     </div>
- <?php   echo '<div id="columnB">';
- print "<h2>Ilo¶æ ksi±¿ek w bazie</h2>";
- print "<p>";
- while ($rekord = mysql_fetch_array ($iloscksiazek)) {
- $id_ksiazka = $rekord[0];
- print "<h2>$id_ksiazka szt.</h2> \n";
- }
- 
- mysql_close ($link);
-?>
-      </p>
-		<p><br />
-<h2>Kontakt</h2>
-<ul>
-<li>Biblioteka Miejska nr.3</a></li>
-<li>123-321 Czêstochowa</li>
-<li>Ul. Korfantego 34a</a></li>
-<li>Tel. +48 123-456-789</li>
-</ul>
-</p>
-    </div>
-    <div style="clear: both;">&nbsp;</div>
+    <?php   echo '<div id="columnB">';
+    print "<h2>Ilo¶æ ksi±¿ek w bazie</h2>";
+    print "<p>";
+    while ($rekord = mysql_fetch_array ($iloscksiazek)) {
+    $id_ksiazka = $rekord[0];
+    print "<h2>$id_ksiazka szt.</h2> \n";
+    }
+    
+    mysql_close ($link);
+    ?></p>
+    <p><br />
+    <h2>Kontakt</h2>
+    <ul>
+      <li>Biblioteka Miejska nr.3</a></li>
+      <li>123-321 Czêstochowa</li>
+      <li>Ul. Korfantego 34a</a></li>
+      <li>Tel. +48 123-456-789</li>
+    </ul>
+    </p>
+  </div>
+  <div style="clear: both;">&nbsp;</div>
   </div>
   <div id="footer">
     <p><a href="http://www.kropleinternetu.biz/biblioteka" class="link1"><b>bdMAK</b></a>Copyright &copy; 2011<a href="http://www.kropleinternetu.biz/biblioteka" class="link1">Biblioteka Miejska nr.3 Demo</a></p>
