902c29ffe6bfe8e7680fd82175c8a48215b0d142
Mariusz <m.purgal@gmail.com>
2011-05-15 08:34:15 +0200

    Plik pomocy dla internautow
    Signed-off-by: Mariusz <m.purgal@gmail.com>

---
+++ webhelp.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!--
+Tytuł ; Biblioteczka MAK - System zarządzania - Biblioteka
+Opis  ; Strona pomocy dla internautow.
+Data  ; Maj 2011
+Autor ; Mariusz P., Agnieszka S., Krzysztof P.
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-2" />
+<title>System zarządzania - Biblioteka POMOC</title>
+<meta name="Author" content="M.P., A.S., K.P." />
+<meta name="Robots" content="noindex, nofollow" />
+<link href="default.css" rel="stylesheet" type="text/css" />
+</head>
+<body style="margin:20px 20px 20px 20px;">
+<noscript>
+<p class="noscriptText"><b>Koniecznie włącz obsługę JavaScript.</b><a href="http://www.kropleinternetu,biz/obslugajava.php" title="Włączenie odsługi JavaScript w przeglądarkach internetowych." target="_blank" style="text-decoration:blink;"><b> Kliknij i sprawdź jak to zrobić&nbsp;&nbsp;&raquo;</b></a></p>
+</noscript>
+<h1>Pomoc</h1>
+<br/>
+<h3>1. Wyszukiwanie książek</h3>
+<p>&nbsp;</p>
+<h3>2. Rezerwacja</h3>
+<p>&nbsp;</p>
+</body>
+</html>
